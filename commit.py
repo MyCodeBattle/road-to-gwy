f71aec84ff825a53fd134b4536a1bf2961231418
PureFrog <mycodebattle@outlook.com>
2019-11-20 09:47:19 +0800

    commit

--- commit.py
+++ commit.py
@@ -1,6 +1,6 @@
 import os
 os.system('gitbook build 正文/')
-os.system('cp -r 正文/_book .')
+os.system('cp -r 正文/_book/* .')
 os.system('cp -r _book/* .')
 os.system('git add .')
 os.system('git commit -m commit')
