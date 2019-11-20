9bfe472ef574cc5ec4c292afa6feae8d436b9386
PureFrog <mycodebattle@outlook.com>
2019-11-21 00:07:50 +0800

    commit

--- commit.py
+++ commit.py
@@ -1,6 +1,6 @@
 import os
 os.system('gitbook build 冰熊手册/')
-os.system('mv 冰熊手册/_book/* .')
+os.system('cp -r 冰熊手册/_book/* .')
 os.system('git add .')
 os.system('git commit -m commit')
 os.system('git push')
